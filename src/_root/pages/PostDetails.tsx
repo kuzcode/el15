27a2f613dcf77868a8612fb2433b6ed4be056183
Nikita Kuzmin <nikita.kuzmaaa@gmail.com>
2024-06-23 13:25:46 +0300

    zhopa chlen piska

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -5,11 +5,17 @@ import { Loader } from "@/components/shared";
 import {
   useGetPostById,
 } from "@/lib/react-query/queries";
+import { useState } from "react";
 const PostDetails = () => {
   const navigate = useNavigate();
   const { id } = useParams();
 
   const { data: post, isLoading } = useGetPostById(id);
+
+  const [form, setForm] = useState({
+    size: 39,
+    color: post?.colors[0],
+  });
   return (
     <div className="">
       <div className="hidden md:flex max-w-5xl w-full">
@@ -30,33 +36,51 @@ const PostDetails = () => {
       {isLoading || !post ? (
         <Loader />
       ) : (
-        <div className="">
+        <div className="pb-[100px]">
           <img
             src={post.imageUrl}
             className="w-[100%] rounded-[12px]"
           />
 
-          <h1 className="text-[32px] font-bold mx-3">{post.caption} - {post.price}BYN</h1>
-          <h1 className="text-[32px] font-bold">{post.description}</h1>
+          <h1 className="text-[32px] font-bold mx-3">{post.caption} - <span className="text-[#505050] text-[28px] font-regular">{post.price}BYN</span></h1>
+          <h1 className="text-[28px] font-regular mx-3 text-[#616161]">{post.description}</h1>
+
+          <h1 className="text-[32px] font-bold mx-3">Цвета</h1>
 
           <div className="px-[10px] mt-[4px] flex flex-row">
             {post.colors.map((color: any) => (
-              <div style={{
-                backgroundColor: `#${color}`,
+              <div
+              onClick={() => {
+                setForm({...form, color: color})
               }}
-                className="w-[28px] h-[28px] mx-[2px] border-[2px] border-[#00000008] rounded-full"
+              style={{
+                backgroundColor: `#${color}`,
+                borderColor: form.color === color ? `#${color}` : '#fff'
+              }}
+                className="w-[36px] h-[36px] mx-[2px] border-[4px] rounded-full"
               ></div>
             ))}
+          </div>
 
-            <p className="text-[17px] text-[#191920af]">{post.colors.length} цвет</p>
+          <h1 className="text-[32px] font-bold mx-3">Размеры</h1>
 
             <div className="px-[10px] mt-[4px] flex flex-row">
             {post.sizes.map((size: any) => (
-              <div className="bg-white px-5 py-2 border-[2px] border-[#000] rounded-[10px]">
-              <p className="text-[24px]">{size}</p>
+              <div className="mr-[4px] px-5 py-2 border-[2px] border-[#000] rounded-[14px]"
+              onClick={() => {
+                setForm({...form, size: size})
+              }}
+              style={{
+                backgroundColor: form.size === size ? '#000' : '#fff'
+              }}
+              >
+              <p className="text-[24px]"
+              style={{
+                color: form.size === size ? '#fff' : '#000'
+              }}
+              >{size}</p>
               </div>
             ))}
-          </div>
           </div>
         </div>
       )}
